be6b296fc2499de1ecb849c6240e0476ee59e088
Daniil Dumshenko <dumshenko2003@gmail.com>
2025-01-14 08:59:50 +0100

    Ping_controller

---
+++ src/main/java/dumshenko/daniil/todo_list_demo/PingController.java
@@ -0,0 +1,12 @@
+package dumshenko.daniil.todo_list_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
